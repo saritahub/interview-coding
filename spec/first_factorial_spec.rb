b360fd520ae97684a0f7aea161dcd29669305ff1
Sarita <saritaradia65@gmail.com>
2023-01-12 15:04:09 +0000

    green test / refactor: Returns 1 when the input is 1

--- spec/first_factorial_spec.rb
+++ spec/first_factorial_spec.rb
@@ -5,8 +5,7 @@ require 'first_factorial'
 RSpec.describe 'First Factorial' do 
     context 'Expected behavior' do 
         it 'Returns 1 when the input is 1' do 
-            result = FirstFactorial(1)
-            expect(result).to eq(1)
+            expect(FirstFactorial(1)).to eq(1)
         end 
     end
 end 
