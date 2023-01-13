177bb42bcc2248c5c345558bd941805655e0e8f3
Sarita <saritaradia65@gmail.com>
2023-01-13 15:27:23 +0000

    refactor if statement

--- lib/bracket_matcher.rb
+++ lib/bracket_matcher.rb
@@ -1,5 +1,23 @@
 # File: lib/bracket_matcher.rb 
 
 def BracketMatcher(str)
-    return 1
+    right_bracket = []
+    left_bracket = []
+
+    strArr = str.split("")
+    strArr.map do |element|
+        if element == "("
+            right_bracket.push(element)
+        elsif element == ")"
+            left_bracket.push(element)
+        end
+    end 
+
+    if (!right_bracket.empty? && !left_bracket.empty?) && (right_bracket.length == left_bracket.length)
+        "1"
+    else 
+        "0"
+    end 
+
+
 end
--- spec/bracket_matcher_spec.rb
+++ spec/bracket_matcher_spec.rb
@@ -5,15 +5,15 @@ require 'bracket_matcher'
 RSpec.describe 'BracketsMatched' do 
     context 'Expected behavior' do 
         it 'Returns 1 when an empty set of brackets is passed through' do 
-            expect(BracketMatcher("()")).to eq(1)
+            expect(BracketMatcher("()")).to eq("1")
         end 
 
         it 'Returns 1 when (hello) is passed through' do 
-            expect(BracketMatcher("(hello)")).to eq(1)
+            expect(BracketMatcher("(hello)")).to eq("1")
         end 
 
         it 'Returns 1 when (c(oder)) b(yte) is passed through' do 
-            expect(BracketMatcher("(c(oder)) b(yte)")).to eq(1)
+            expect(BracketMatcher("(c(oder)) b(yte)")).to eq("1")
         end 
 
     end 
