607f8cb0e2342bbfa4edcbd96d1235dc6b6957f9
Sarita <saritaradia65@gmail.com>
2023-01-13 16:19:05 +0000

    refactor: removed whitespace

--- lib/username_verification.rb
+++ lib/username_verification.rb
@@ -7,5 +7,4 @@ def CodelandUsernameValidation(str)
     requirements = str.length.between?(4, 25) && str[0].match(letters) != nil && str[-1] != "_" && str.match(allowed_chars) != nil
 
     requirements ? true : false 
-
 end
