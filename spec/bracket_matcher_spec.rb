4a02934ce224a80ab8c7f37c5776b8130f89f0d4
Sarita <saritaradia65@gmail.com>
2023-01-13 15:20:45 +0000

    green test: Returns 1 when (hello) is passed through

--- spec/bracket_matcher_spec.rb
+++ spec/bracket_matcher_spec.rb
@@ -4,9 +4,15 @@ require 'bracket_matcher'
 
 RSpec.describe 'BracketsMatched' do 
     context 'Expected behavior' do 
-        it "Returns 1 when an empty set of brackets is passed through" do 
+        it 'Returns 1 when an empty set of brackets is passed through' do 
             expect(BracketMatcher("()")).to eq(1)
         end 
+
+        it 'Returns 1 when (hello) is passed through' do 
+            expect(BracketMatcher("(hello)")).to eq(1)
+        end 
+
+        
     end 
 end 
 
