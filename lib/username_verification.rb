d77baeb7e274aae69b114da0cfe47f4c7e3ade1b
Sarita <saritaradia65@gmail.com>
2023-01-13 16:14:51 +0000

    refactor if statement

--- lib/username_verification.rb
+++ lib/username_verification.rb
@@ -4,9 +4,8 @@ def CodelandUsernameValidation(str)
     letters = /^[A-Za-z]+$/
     allowed_chars = /^[A-Za-z, _, [+-]?\d+]+$/
 
-    if str.length.between?(4, 25) && str[0].match(letters) != nil && str[-1] != "_" && str.match(allowed_chars) != nil
-        return true 
-    else 
-        return false 
-    end
+    requirements = str.length.between?(4, 25) && str[0].match(letters) != nil && str[-1] != "_" && str.match(allowed_chars) != nil
+
+    requirements ? true : false 
+
 end
