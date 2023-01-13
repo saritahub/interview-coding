187c44c5fcdaf9a4d9d6efc9a6782d57fc8d93bd
Sarita <saritaradia65@gmail.com>
2023-01-13 11:48:45 +0000

    refactor

--- lib/questionmarks.rb
+++ lib/questionmarks.rb
@@ -1,26 +1,17 @@
 # File: lib/questionmarks.rb
 
 def QuestionsMarks(str)
-    if str.length > 1
-        remove_letters = str.gsub(/[^0-9,?,.]/, "")
-            if remove_letters[0] == "?"
-                first = remove_letters[1].to_i
-                second = remove_letters[5].to_i
-            else 
-                first = remove_letters[0].to_i
-                second = remove_letters[4].to_i
-            end 
-            if first + second == 10
-                return true 
-            else 
-                return false 
-            end 
+    remove_letters_str = str.gsub(/[^0-9,?,.]/, "")
+   
+    if remove_letters_str[0].to_i == 0
+        first = remove_letters_str[1].to_i
+        second = remove_letters_str[5].to_i
     else 
-        return false
-    end
-end
-
-
-
+        first = remove_letters_str[0].to_i
+        second = remove_letters_str[4].to_i
+    end 
+           
+    first + second == 10 ? true : false
+end 
 
 
