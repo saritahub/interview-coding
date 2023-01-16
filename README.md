2e4aa45c1c5475edfd0d059c94272df2a66038f2
Sarita <saritaradia65@gmail.com>
2023-01-16 11:44:00 +0000

    Created MinWindowSubstring spec and README files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Code challenge practice 
+
+Using a TDD approach, I have completed code challenges from: 
+1. CoderByte 
---
+++ coderbyte/spec/MinWindowSubstring_spec.rb
@@ -0,0 +1,15 @@
+# File: spec/MinWindowSubstring_spec.rb
+
+# require 'MinWindowSubstring'
+
+RSpec.describe 'MinWindowSubstring' do 
+    context 'Expected behavior' do 
+    end 
+end 
+
+# def MinWindowSubstring(strArr)
+
+#     # code goes here
+#     return strArr
+  
+# end
