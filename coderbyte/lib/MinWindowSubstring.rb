bdbbb00b680f3b3cbddbf8d818fada3d9e73334d
Sarita <saritaradia65@gmail.com>
2023-01-16 12:37:05 +0000

    refactor

--- coderbyte/lib/MinWindowSubstring.rb
+++ coderbyte/lib/MinWindowSubstring.rb
@@ -1,9 +1,24 @@
 # File: lib/MinWindowSubstring.rb
 
 def MinWindowSubstring(strArr)
-    string_N = strArr[0]
-    string_K = strArr[1]
+   first_array = strArr[0].split("")
+   second_array = strArr[1].split("")
 
-    return string_N && string_K
+   @return_array = []
+
+   intersection = first_array.intersection(second_array)
+
+   letters_to_delete = first_array - intersection
+
+   p "Intersection: #{intersection}, remove: #{letters_to_delete}"
+
+   first_array.map { |element|
+            if intersection.include?(element)
+                @return_array.push(element)
+            end 
+   }
+
+   @return_array.join("")
+
 
 end
